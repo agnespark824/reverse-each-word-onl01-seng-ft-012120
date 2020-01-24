542b73394384369e6aa6c9ba7c58d9110fb963eb
Agnes Park <agnespark824@gmail.com>
2020-01-24 01:58:04 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   array_one.collect do |word|
     word.reverse
   end
-  array_one.join(" ")
+  puts array_one.join(" ")
 end
 
 reverse_each_word("Hello, how are you?")
